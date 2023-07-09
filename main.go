6b9de8f365766cc53fc994c6485968f72013ec11
Alit Djoin <alit.indrawan@djoin.id>
2023-07-10 00:16:47 +0800

    fix: add method label in latency metrics

--- main.go
+++ main.go
@@ -22,29 +22,43 @@ var (
 			// Define the desired histogram buckets.
 			Buckets: []float64{0.1, 0.3, 0.5, 0.7, 0.9},
 		},
-		[]string{"path"},
+		[]string{"method", "path"},
 	)
 )
 
+// RecordHttpRequest records an HTTP request.
+
+// This function takes the HTTP method, path, and response code of an HTTP request
+// as input parameters. It increments the appHttpRequest metric with the provided
+// method, path, and code labels.
 func RecordHttpRequest(method string, path string, code int) {
 	appHttpRequest.WithLabelValues(method, path, strconv.Itoa(code)).Inc()
 }
 
-func RecordLatency(path string, start time.Time) {
+// RecordLatency records the latency of an HTTP request.
+//
+// This function takes the HTTP method, path, and the start time of the request
+// as input parameters. It calculates the elapsed time since the start time
+// and records the latency using the appHttpLatency metric.
+func RecordLatency(method string, path string, start time.Time) {
 	elapsed := time.Since(start).Seconds()
-	appHttpLatency.WithLabelValues(path).Observe(elapsed)
+	appHttpLatency.WithLabelValues(method, path).Observe(elapsed)
 }
 
+// MetricCollector is a middleware function for collecting metrics in an Echo framework application.
+//
+// This function returns a middleware function that wraps the next handler function in the Echo
+// framework's middleware chain. It measures the time taken to process the request and records
+// relevant metrics including the HTTP request and latency.
 func MetricCollector() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			start := time.Now()
-
 			request := next(c)
 
 			// Record metrics
 			RecordHttpRequest(c.Request().Method, c.Path(), c.Response().Status)
-			RecordLatency(c.Path(), start)
+			RecordLatency(c.Request().Method, c.Path(), start)
 
 			return request
 		}
